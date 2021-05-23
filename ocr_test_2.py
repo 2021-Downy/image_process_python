5576f96d5965904d96a49e97ddfb0029da92f7eb
SeoHyeon Ryu <ryuish541@gmail.com>
2021-05-23 16:18:11 +0900

    get img from url(example)

--- ocr_test_2.py
+++ ocr_test_2.py
@@ -1,8 +1,17 @@
 import cv2
 import pytesseract
+import numpy as np
+import requests
 
-img = cv2.imread('example4.png',cv2.IMREAD_GRAYSCALE)
+# 다운받을 이미지 url
+url = "https://dispatch.cdnser.be/cms-content/uploads/2020/04/09/a26f4b7b-9769-49dd-aed3-b7067fbc5a8c.jpg"
 
-# Adding custom options
+image_nparray = np.asarray(bytearray(requests.get(url).content), dtype=np.uint8)
+
+image = cv2.imdecode(image_nparray, cv2.IMREAD_GRAYSCALE)
+print(image.shape)
 custom_config = r'--oem 3 --psm 6'
-print(pytesseract.image_to_string(img, lang="kor", config=custom_config))
+print(pytesseract.image_to_string(image, lang="kor", config=custom_config))
+cv2.imshow('Image from url', image)
+cv2.waitKey(0)
+cv2.destroyAllWindow()
